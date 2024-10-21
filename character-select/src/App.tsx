ebdb9a28c9f02c51898e7232137f8dc2c41d083c
Djafny <jafnyabdad@gmail.com>
2024-10-21 14:33:14 +0200

    feat: added background image

--- character-select/src/App.tsx
+++ character-select/src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css';
+import '@mantine/core/styles.css';
+import { MantineProvider, BackgroundImage, Center, Text, Box } from '@mantine/core';
+import image from './assets/img/db.jpeg';
 
 const App = () => {
   return (
-    <>
-      <p>sadsadsadasd</p>
-    </>
+    <MantineProvider>
+      <Box style={{ height: '100vh', width: '100vw' }} mx="auto">
+        <BackgroundImage
+          src={image}
+          style={{ height: '100%', width: '100%' }}
+
+        >
+          <Center p="md">
+            {/* You can add content here if needed */}
+            <Text color="white" size="xl">Welcome to My App</Text>
+          </Center>
+        </BackgroundImage>
+      </Box>
+    </MantineProvider>
   );
 }
 
