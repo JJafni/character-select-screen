2b29a7ce82dda2ce32ea20b227e4dd4831f42e0e
Djafny <jafnyabdad@gmail.com>
2024-10-24 10:42:40 +0200

    feat: temporary -backgrounf

--- character-select/src/App.tsx
+++ character-select/src/App.tsx
@@ -3,8 +3,8 @@ import '@mantine/core/styles.css';
 import { MantineProvider, Image, Box, SimpleGrid } from '@mantine/core';
 
 
-import { useMediaQuery } from '@mantine/hooks'; // Import useMediaQuery
 
+import { useMediaQuery } from '@mantine/hooks'; // Import useMediaQuery
 import character from './data/dbdata'; // Importing character data
 
 const App = () => {
@@ -18,7 +18,17 @@ const App = () => {
     <MantineProvider>
 
       {/* Allow the Box to expand based on content */}
-      <Box style={{ minHeight: '100vh', width: '100vw', overflowY: 'auto', }} mx="auto">
+      <Box style={{
+         minHeight: '100vh',
+         width: '100vw',
+         overflowY: 'auto',
+         backgroundImage: 'url(https://i.ibb.co/KF3tpzt/db.jpg)', // Path to your background image
+         backgroundSize: 'cover',       // Ensures the image covers the entire area without distortion
+          backgroundPosition: 'center',  // Centers the image
+          backgroundRepeat: 'no-repeat', // Prevents the image from repeating
+          backgroundAttachment: 'fixed', // Keeps the background fixed while scrolling for a better visual effect
+          filter: 'brightness(0.9)',     // Optional: Adjust brightness for readability
+      }} mx="auto">
 
         {/* Use SimpleGrid for responsive layout */}
         <SimpleGrid
@@ -30,7 +40,7 @@ const App = () => {
           {character.map((char) => (
             <Image
               key={char.name}        // Use a unique key for each image
-              height={500}           // Use numeric value for height
+              height={'100%'}           // Use numeric value for height
               src={char.path}        // Path to the character image
               alt={char.name}        // Alt text for accessibility
               fit="contain"          // Ensure the image fits well within its space
