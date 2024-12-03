a0b9c3394759f82541a2bcf5eeaa7177c06ee679
Djafny Abdad <bakso@Djafnys-MacBook-Air.local>
2024-12-03 14:04:35 +0100

    feat: code structure

--- character-select/src/App.tsx
+++ character-select/src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
                 <BoxMain>
                   <CharacterGrid cols={cols} characters={characterData} />
                 </BoxMain>
-                <DockSaga />
+                {/* <DockSaga /> */}
               </>
             }
           />
